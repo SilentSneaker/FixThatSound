7947e6d76415352ee33bc789e95c00dcccfa4d33
SilentSneaker <98756551+SilentSneaker@users.noreply.github.com>
2023-01-17 23:11:25 -0500

    1/17/2023

--- src/main/java/com/acrcloud/acrclouduniversalsdkdemo/Maintenance.java
+++ src/main/java/com/acrcloud/acrclouduniversalsdkdemo/Maintenance.java
@@ -1,5 +1,6 @@
 package com.acrcloud.acrclouduniversalsdkdemo;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,41 +8,109 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 
 public class Maintenance extends AppCompatActivity {
 
 
-    TextView oil = findViewById(R.id.OilTextDate);
+    static int changeOil, changeAirFilter,changeTransmission;
     private int oilDay, oilMonth, oilYear;
+    private int transmissionDay,transmissionMonth,transmissionYear;
+    private int airFilterDay, airFilterMonth, airFilterYear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maintenance);
-        oil.setText("Last oil change was: "+ LocalDate.now().minusDays(oilDay).minusMonths(oilMonth).minusYears(oilYear));
+        TextView oil = findViewById(R.id.OilTextDate);
+        TextView transmission = findViewById(R.id.transmission);
+        TextView airFilter = findViewById(R.id.battery);
 
-
-    }
-
-    void pullUpCalendar(View view) {
-        final Calendar calendar = Calendar.getInstance();
-        oilDay = calendar.get(Calendar.DATE);
-        oilMonth = calendar.get(Calendar.MONTH);
-        oilYear = calendar.get(Calendar.YEAR);
-        DatePickerDialog datePickerDialog = new DatePickerDialog(Maintenance.this, android.R.style.Theme_DeviceDefault_Dialog, new DatePickerDialog.OnDateSetListener() {
+        oil.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                oil.setText("Last oil change was: "+ LocalDate.now().minusDays(oilDay).minusMonths(oilMonth).minusYears(oilYear));
-                oilYear = year;
-                oilDay = day;
-                oilMonth = month;
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                oilDay = calendar.get(Calendar.DATE);
+                oilMonth = calendar.get(Calendar.MONTH);
+                oilYear = calendar.get(Calendar.YEAR);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Maintenance.this, android.R.style.Theme_DeviceDefault_Dialog, new DatePickerDialog.OnDateSetListener() {
+                    @SuppressLint("SuspiciousIndentation")
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        Date a = new Date(year, month, day);
+                        Date b = new Date(LocalDate.now().getYear(), LocalDate.now().getMonthValue(), LocalDate.now().getDayOfMonth());
+                        Long c = ((b.getTime()-a.getTime())/1000/60/60/24) - 31;
+                        changeOil = Math.toIntExact(c);
+                        oilYear = year;
+                        oilDay = day;
+                        oilMonth = month;
+                        if (changeOil < 90)
+                        oil.setText("Last oil change was "+ c + " Day(s) ago.");
+                        else
+                        oil.setText("Last oil change was "+ c + " Day(s) ago and needs to be changed");
+
+                    }
+                }, oilYear, oilMonth, oilDay );
+                datePickerDialog.show();
             }
-        }, oilYear, oilMonth, oilDay );
-        datePickerDialog.show();
+        });
+        transmission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                transmissionDay = calendar.get(Calendar.DATE);
+                transmissionMonth = calendar.get(Calendar.MONTH);
+                transmissionYear = calendar.get(Calendar.YEAR);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Maintenance.this, android.R.style.Theme_DeviceDefault_Dialog, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        Date a = new Date(year, month, day);
+                        Date b = new Date(LocalDate.now().getYear(), LocalDate.now().getMonthValue(), LocalDate.now().getDayOfMonth());
+                        Long c = ((b.getTime()-a.getTime())/1000/60/60/24) - 31;
+                        changeTransmission = Math.toIntExact(c);
+                        transmissionYear = year;
+                        transmissionDay = day;
+                        transmissionMonth = month;
+                        if (changeTransmission < 1460)
+                            transmission.setText("Last Transmission Fluid change was "+ c + " Day(s) ago.");
+                        else
+                            transmission.setText("Last Transmission Fluid change was "+ c + " Day(s) ago and needs to be changed");
+
+                    }
+                }, transmissionYear, transmissionMonth, transmissionDay );
+                datePickerDialog.show();
+            }
+        });
+        airFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                airFilterDay = calendar.get(Calendar.DATE);
+                airFilterMonth = calendar.get(Calendar.MONTH);
+                airFilterYear = calendar.get(Calendar.YEAR);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Maintenance.this, android.R.style.Theme_DeviceDefault_Dialog, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        Date a = new Date(year, month, day);
+                        Date b = new Date(LocalDate.now().getYear(), LocalDate.now().getMonthValue(), LocalDate.now().getDayOfMonth());
+                        Long c = ((b.getTime()-a.getTime())/1000/60/60/24) - 31;
+                        changeAirFilter = Math.toIntExact(c);
+                        airFilterYear = year;
+                        airFilterDay = day;
+                        airFilterMonth = month;
+                        if (changeAirFilter<180)
+                        airFilter.setText("Last Air Filter change was "+ c + " Day(s) ago.");
+                        else
+                            airFilter.setText("Last Air Filter change was "+ c + " Day(s) ago and needs to be changed");
+
+
+                    }
+                }, airFilterYear, airFilterMonth, airFilterDay);
+                datePickerDialog.show();
+            }
+        });
+
     }
 
 
--- src/main/java/com/acrcloud/acrclouduniversalsdkdemo/ResultScreen.java
+++ src/main/java/com/acrcloud/acrclouduniversalsdkdemo/ResultScreen.java
@@ -1,7 +1,9 @@
 package com.acrcloud.acrclouduniversalsdkdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class ResultScreen extends AppCompatActivity {
@@ -13,5 +15,9 @@ public class ResultScreen extends AppCompatActivity {
         setContentView(R.layout.activity_result_screen);
         TextView result = (TextView) findViewById(R.id.result);
         result.setText(MainActivity.Result);
+    }
+
+    public void Profile(View view) {
+        startActivity(new Intent(ResultScreen.this, Maintenance.class));
     }
 }
